bec8fcabebb9ffd346719837cc1f06854ad57eab
ILLUSIONack <usman-ack@hotmail.com>
2018-11-18 22:55:55 +0100

    changed user variable in grid

--- TicTacToe.py
+++ TicTacToe.py
@@ -7,18 +7,16 @@ grid = [[1, 2, 3],
         [7, 8, 9]]
 
 
-def is_valid_location(grid,position_choose):
-    print(position_choose)
+def is_valid_location(grid, position_choose):
     for y in range(3):
         for x in range(3):
             if grid[y][x] == position_choose:
-                print(grid[0][0])
                 return position_choose
 
     return True
 
+
 print(grid)
-print(grid[0][0])
 turn = 0
 game_over = False
 
@@ -33,28 +31,27 @@ while not game_over:
             is_valid_location(grid, position_choose)
 
         if position_grid == 1:
-            grid[0][0] = "x"
+            grid[0][0] = 10
         elif position_grid == 2:
-            grid[0][1] = "x"
+            grid[0][1] = 10
         elif position_grid == 3:
-            grid[0][2] = "x"
+            grid[0][2] = 10
         elif position_grid == 4:
-            grid[1][0] = "x"
+            grid[1][0] = 10
         elif position_grid == 5:
-            grid[1][1] = "x"
+            grid[1][1] = 10
         elif position_grid == 6:
-            grid[1][2] = "x"
+            grid[1][2] = 10
         elif position_grid == 7:
-            grid[2][0] = "x"
+            grid[2][0] = 10
         elif position_grid == 8:
-            grid[2][1] = "x"
+            grid[2][1] = 10
         elif position_grid == 9:
-            grid[2][2] = "x"
-        print(grid)
+            grid[2][2] = 10
+    print(grid)
 
     if turn == 1:
         # Player input 2
-        print("hello")
         position_choose = int(input("Choose a position to draw in from 1-9"))
         is_valid_location(grid, position_choose)
         position_grid = is_valid_location(grid, position_choose)
@@ -63,23 +60,23 @@ while not game_over:
             is_valid_location(grid, position_choose)
 
         if position_grid == 1:
-            grid[0][0] = "o"
+            grid[0][0] = 11
         elif position_grid == 2:
-            grid[0][1] = "o"
+            grid[0][1] = 11
         elif position_grid == 3:
-            grid[0][2] = "o"
+            grid[0][2] = 11
         elif position_grid == 4:
-            grid[1][0] = "o"
+            grid[1][0] = 11
         elif position_grid == 5:
-            grid[1][1] = "o"
+            grid[1][1] = 11
         elif position_grid == 6:
-            grid[1][2] = "o"
+            grid[1][2] = 11
         elif position_grid == 7:
-            grid[2][0] = "o"
+            grid[2][0] = 11
         elif position_grid == 8:
-            grid[2][1] = "o"
+            grid[2][1] = 11
         elif position_grid == 9:
-            grid[2][2] = "o"
+            grid[2][2] = 11
         print(grid)
     turn = turn + 1
     turn = turn % 2
