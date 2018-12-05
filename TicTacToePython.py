911cd26ddc849f9c595757733003de7e8d1b97a0
ILLUSIONack <usman-ack@hotmail.com>
2018-12-05 01:23:14 +0100

    Cleaned up

--- TicTacToePython.py
+++ TicTacToePython.py
@@ -1,15 +1,43 @@
 from turtle import *
 
+# Turtle Properties
 speed(0)
 color('black')
-setup(600,600)
+setup(600, 600)
 pensize(10)
 
-penup()
-goto(-300,-100)
-pendown()
+# Grid positions x and y
+positions = {
+    1: (-215, 205),
+    2: (-12, 209),
+    3: (188, 208),
+    4: (-218, 4),
+    5: (0, 0),
+    6: (192, 3),
+    7: (-220, -199),
+    8: (-9, -201),
+    9: (194, -195),
+}
 
-def board():
+# Empty positions
+grid = []
+
+
+def is_valid_location(grid, user_input):
+    if user_input not in grid:
+        grid.append(user_input)
+        print(grid)
+        return user_input
+    else:
+        while True:
+            user_input = int(numinput("Your turn", "Pick again, choose a position 1-9", 1, minval=1, maxval=9))
+            return is_valid_location(grid, user_input)
+
+
+def draw_board():
+    penup()
+    goto(-300, -100)
+    pendown()
     for x in range(2):
         fd(600)
         left(90)
@@ -18,19 +46,21 @@ def board():
         left(90)
         pendown()
     penup()
-    
-    goto(-100,-300)
+
+    goto(-100, -300)
     left(90)
     pendown()
-    fd(600)
-    right(90)
+    for i in range(2):
+        fd(600)
+        right(90)
+        penup()
+        fd(200)
+        right(90)
+        pendown()
     penup()
-    fd(200)
-    right(90)
-    pendown()
-    fd(600)
 
-def square_red():
+
+def player_red():
     pendown()
     begin_fill()
     color('red')
@@ -40,7 +70,8 @@ def square_red():
     penup()
     end_fill()
 
-def square_blue():
+
+def player_blue():
     pendown()
     begin_fill()
     color('blue')
@@ -51,38 +82,30 @@ def square_blue():
     end_fill()
 
 
-positions = {
-    1: (-215,205),
-    2: (-12, 209),
-    3: (188, 208),
-    4: (-218, 4),
-    5: (0,0),
-    6: (192,3),
-    7: (-220,199),
-    8: (-9,201),
-    9: (194, -195),
-    }
+def draw_token_on_board(func, player_turn):
+    if player_turn == 0:
+        goto(positions[func])
+        player_blue()
+    elif player_turn == 1:
+        goto(positions[func])
+        player_red()
 
-board()
-penup()
-turn = 0
-for turn in range(9):
-    if turn == 0:
-        user_position = numinput("Your turn", "choose a position 1-9", 1, minval=1, maxval=9)
-        goto(positions[user_position])
-        square_blue()
-    turn+=1
 
-    if turn == 1:
-        user_position = numinput("Your turn", "choose a position 1-9", 1, minval=1, maxval=9)
-        goto(positions[user_position])
-        square_red()
-    (turn%2)-1
-    print(turn)
+def game_loop():
+    for turn in range(1,10):
+        turn = turn % 2
+        if turn == 0:
+            user_position = int(numinput("Your turn", "choose a position 1-9", 1, minval=1, maxval=9))
+            draw_token_on_board(is_valid_location(grid, user_position), turn)
 
-        
-        
+        elif turn == 1:
+            user_position = int(numinput("Your turn", "choose a position 1-9", 1, minval=1, maxval=9))
+            draw_token_on_board(is_valid_location(grid, user_position), turn)
 
-        
-    
+
+draw_board()
+game_loop()
+
+
+
 
